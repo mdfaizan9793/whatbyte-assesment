2fdc772046c47bc3aded40f95c03f1800ac84cf4
Mohammad Faizan <mdfaizanaslamsidd7862@gmail.com>
2024-09-26 11:49:28 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full flex items-center justify-center text-2xl">
+      Please go to skill test
+    </div>
+  );
+}
